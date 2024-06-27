e6a44e867c5dfc6cbaea63aaa0e9fdff98fea1bb
Shivan Kathir <shivan.kathir@gmail.com>
2024-06-27 18:56:30 -0400

    finished the hours section

--- src/homepage.js
+++ src/homepage.js
@@ -16,9 +16,32 @@ messageContainer.appendChild(para);
 const hour =document.createElement("div");
 const hourHeader = document.createElement("h2");
 hourHeader.textContent="Hours";
-const hourPara = document.createElement("p");
+const hourContainer = document.createElement("div");
+
+const hour1 = document.createElement("p");
+hour1.textContent="Sunday: 8am - 8pm";
+const hour2 = document.createElement("p");
+hour2.textContent="Monday: 6am - 6pm";
+const hour3 = document.createElement("p");
+hour3.textContent="Tuesday: 6am - 6pm";
+const hour4 = document.createElement("p");
+hour4.textContent="Wednesday: 6am - 6pm";
+const hour5 = document.createElement("p");
+hour5.textContent="Thursday: 6am - 10pm";
+const hour6 = document.createElement("p");
+hour6.textContent="Friday: 6am - 10pm";
+const hour7 = document.createElement("p");
+hour7.textContent="Saturday: 8am - 10pm";
+hourContainer.appendChild(hour1);
+hourContainer.appendChild(hour2);
+hourContainer.appendChild(hour3);
+hourContainer.appendChild(hour4);
+hourContainer.appendChild(hour5);
+hourContainer.appendChild(hour6);
+hourContainer.appendChild(hour7);
+
 hour.appendChild(hourHeader);
-hour.appendChild(hourPara);
+hour.appendChild(hourContainer);
 
 const location = document.createElement("div");
 const locationHeader = document.createElement("h2");
